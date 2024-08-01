2c6ac1c8a25a6faea85eae0a90b4c123dde8f170
kangyeongmin <kym6050@hanyang.ac.kr>
2024-08-01 15:48:31 +0900

    feat: Badge구현

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,22 @@
 import { createFileRoute } from "@tanstack/react-router";
+import Badge from "../components/Badge";
+import styled from "styled-components";
 
 export const Route = createFileRoute("/")({
   component: Home,
 });
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <Wrapper>
+      <Badge text="Welcome to Chattravel!" />
+      Home
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+`;
 
 export default Home;
