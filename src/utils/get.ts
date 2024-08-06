d1ed1a29e85682a93c8dd5999bbe34d168943b8e
kangyeongmin <kym6050@hanyang.ac.kr>
2024-08-06 13:06:20 +0900

    feat: tourApi 기본 params 세팅

--- src/utils/get.ts
+++ src/utils/get.ts
@@ -1,7 +1,8 @@
 import { tourApi } from "../apis/api";
 
 export const getTourPhotos = async () => {
-  const res = await tourApi.get("/");
+  const res = await tourApi.get("/galleryList1");
+
   if (!res) throw new Error("Failed to fetch tour-photos");
   return res.data.json();
 };
