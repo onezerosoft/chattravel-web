fae991ae812250efbb1fa6e17f1636bb10461233
kangyeongmin <kym6050@hanyang.ac.kr>
2024-09-22 23:29:15 +0900

    refactor: early return

--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -10,23 +10,30 @@ const Messages = () => {
   const region = useTravelStore((state) => state.region!);
   const duration = useTravelStore((state) => state.duration);
 
+  if (status == "pending" || !messages) {
+    return (
+      <ChatGroup groupKey={"course"} who="chet">
+        <p>
+          너만을 위한 {REGION_MAP[region]} {DURATIONS[duration - 1]} 여행코스를
+          생성 중이야! <br /> 잠시만 기다려줘~
+        </p>
+        <LoadingDots />
+      </ChatGroup>
+    );
+  }
+
   return (
     <ChatGroup groupKey={"course"} who="chet">
       <p>
         너만을 위한 {REGION_MAP[region]} {DURATIONS[duration - 1]} 여행코스를
         생성 중이야! <br /> 잠시만 기다려줘~
       </p>
-      {status == "pending" ? (
-        <LoadingDots />
-      ) : (
-        messages &&
-        messages.map((message) => (
-          <Course
-            messageId={message.messageId}
-            courses={message.content.courses}
-          />
-        ))
-      )}
+      {messages.map((message) => (
+        <Course
+          messageId={message.messageId}
+          courses={message.content.courses}
+        />
+      ))}
     </ChatGroup>
   );
 };
