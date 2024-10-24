b195511118566c7f58db0a9bdc0fb7a1f12d591a
kangyeongmin <kym6050@hanyang.ac.kr>
2024-10-24 15:01:00 +0900

    feat: 피드백 전송시마다 똑똑지수 최신화 로직

--- src/hooks/usePostFeedback.ts
+++ src/hooks/usePostFeedback.ts
@@ -1,9 +1,14 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { postFeedback } from "../apis/post";
 
 const usePostFeedback = () => {
+  const queryClient = useQueryClient();
+
   const { mutate } = useMutation({
     mutationFn: postFeedback,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["currentScore"] });
+    },
   });
 
   return { mutate };
